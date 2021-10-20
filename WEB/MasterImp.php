248c1c2d2a9c6b0553b9475e812abbb45b52fb2b
hrmartinez86 <hrmartinez86@gmail.com>
2021-10-20 17:05:04 -0500

    ImpresiónDeñ

--- WEB/MasterImp.php
+++ WEB/MasterImp.php
@@ -135,10 +135,10 @@ protected $y0;      // Ordenada de comienzo de la columna
         for ($i=0;$i<count($examArray);$i++)
         {
             if($examArray[$i]['Res']!=''){
-                $this->WriteText($examArray[$i]['Info'],5,0,'',8,'Arial',false,false);
-                $this->WriteText($examArray[$i]['Res'],100,0,'',8,'Arial',false,false);
-                $this->WriteText($examArray[$i]['Res'],100,0,'',8,'Arial',false,false);
-                $this->WriteText($examArray[$i]['um'],140,0,'',8,'Arial',false,false);
+                $this->WriteText(utf8_decode($examArray[$i]['Info']),5,0,'',8,'Arial',false,false);
+                $this->WriteText(utf8_decode($examArray[$i]['Res']),100,0,'',8,'Arial',false,false);
+                $this->WriteText(utf8_decode($examArray[$i]['Res']),100,0,'',8,'Arial',false,false);
+                $this->WriteText(utf8_decode($examArray[$i]['um']),140,0,'',8,'Arial',false,false);
                 if ($examArray[$i]['rt']!="") {
                     $this->WriteText($examArray[$i]['rt'],140,0,'',8,'Arial',false,false);
                 }
