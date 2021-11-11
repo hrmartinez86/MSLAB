50d56c6be49624e27b71eea9339118d0fb5de4cb
hrmartinez86 <hrmartinez86@gmail.com>
2021-11-11 00:43:32 -0600

    agregar ompresion

--- Core/Resultados_r.php
+++ Core/Resultados_r.php
@@ -388,7 +388,7 @@ FROM         lab_relacion_laboratorio_seccion INNER JOIN
             $sql_1 = "EXECUTE SISTEMA_RESULTADOS_WEB_EDIT '" . $VL_Buscar . "'";
           }
           $i = 0;
-           echo $sql_1;
+          //  echo $sql_1;
           $query_result = odbc_exec($db_conn, $sql_1) or
             die("ERROR : No se puede ejecutar la consulta." . odbc_errormsg() . "<br>" . $sql_1);
           if (odbc_num_rows($query_result) != 0) {
@@ -899,12 +899,12 @@ FROM         lab_relacion_laboratorio_seccion INNER JOIN
       </tr>
       <tr>
         <td>
-          <input type="text" id="heCuenta" name="heCuenta" value="<?php  echo $horaEntrega;?>">
+          <input type="hidden" id="heCuenta" name="heCuenta" value="<?php  echo $horaEntrega;?>">
         </td>
       </tr>
       <tr>
         <td>
-          <input type="text" id="fechaIngreso" name="fechaIngreso" value="<?php  echo $VL_Admision;?>">
+          <input type="hidden" id="fechaIngreso" name="fechaIngreso" value="<?php  echo $VL_Admision;?>">
         </td>
       </tr>
     </table>
