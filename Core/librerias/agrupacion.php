707d2210384069f7fd920fa8ce18471d8d110ed1
hrmartinez86 <hrmartinez86@gmail.com>
2021-11-12 22:51:31 -0600

    cambio de conexion

--- Core/librerias/agrupacion.php
+++ Core/librerias/agrupacion.php
@@ -2,7 +2,7 @@
 <?php
 	session_start();
 	$ODBC='laboratorio';
-	$conection = odbc_connect($ODBC, "sa", "") or  die ("<h1 align='center'>ERROR EN LA IDENTIFICACION DE USUARIO EN LA BASE DE DATOS FAVOR DE CONSULTAR A SU PROVEEDOR</h1><br> <a href=\"../index.php\" title=\"Regresar a la pagina principal\" target=\"_self\"><center>REGRESAR</center></a>");
+	$conection = odbc_connect($ODBC, "sa", "Demo123.") or  die ("<h1 align='center'>ERROR EN LA IDENTIFICACION DE USUARIO EN LA BASE DE DATOS FAVOR DE CONSULTAR A SU PROVEEDOR</h1><br> <a href=\"../index.php\" title=\"Regresar a la pagina principal\" target=\"_self\"><center>REGRESAR</center></a>");
     $fecha=date("d/m/Y");
     $hora=date("H:i:s");
     $fh=$fecha." ".$hora; 
