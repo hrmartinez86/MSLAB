33498a64f2b2d34b6eb79d150c973c66b90182ca
hrmartinez86 <hrmartinez86@gmail.com>
2021-11-24 20:54:47 -0600

    cambio de contrasena de Kapi a Demo

--- WEB/paises.php
+++ WEB/paises.php
@@ -2,7 +2,7 @@
 	// Paises
 session_start();
 $ODBC=$_SESSION['ODBC'];
-$conection = odbc_connect($ODBC, "sa", "Kapi312") or  die ("<h1 align='center'>ERROR EN LA IDENTIFICACION DE USUARIO EN LA BASE DE DATOS FAVOR DE CONSULTAR A SU PROVEEDOR</h1><br> <a href=\"../index.php\" title=\"Regresar a la pagina principal\" target=\"_self\"><center>REGRESAR</center></a>");
+$conection = odbc_connect($ODBC, "sa", "Demo123.") or  die ("<h1 align='center'>ERROR EN LA IDENTIFICACION DE USUARIO EN LA BASE DE DATOS FAVOR DE CONSULTAR A SU PROVEEDOR</h1><br> <a href=\"../index.php\" title=\"Regresar a la pagina principal\" target=\"_self\"><center>REGRESAR</center></a>");
 
 
 	$sql="select * from caj_codigos_fonasa";
