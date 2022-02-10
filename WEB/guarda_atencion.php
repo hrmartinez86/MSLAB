bc7d383fc4af581c9127962d9b3220095cc3cfee
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-02-09 22:19:48 -0600

    s

--- WEB/guarda_atencion.php
+++ WEB/guarda_atencion.php
@@ -235,6 +235,7 @@ $query_result = odbc_exec($db_conn, $sql_1) or
 $final = array();
 //rescatamos el listado de peticiones
 $ex = explode(",", $examenes);
+var_dump($ex);
 $j = count($ex);
 //almacenamos el numero consecutivo diario
 $numeroDiario=numeroDiario($fecha)+1;
