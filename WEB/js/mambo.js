104c5cc6a6284064ba3f24a21e8ad5473011103f
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-02-15 23:33:24 -0600

    fecha

--- WEB/js/mambo.js
+++ WEB/js/mambo.js
@@ -76,7 +76,7 @@ function objetoAjax() {
   return xmlhttp;
 }
 function Choose() {
-  var now = new Date();
+  var now = new Date('dd/mm/yyyy');
   console.log(now);
   var dia = now.getDay();
   var mes = now.getMonth() + 1;
