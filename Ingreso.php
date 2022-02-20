d1ff8f571cf17c0d858abd7ade214159ce3527c2
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-02-20 16:17:40 -0600

    grid

--- Ingreso.php
+++ Ingreso.php
@@ -24,10 +24,10 @@ if(isset($_GET['FI']) and isset($_GET['ff']))
 <meta name="description" content="" />
 <link rel="stylesheet" type="text/css" href="WEB/Styles/Core/Style_doctype.css">
 	<!-- Common JS files-->
-<!-- <script type='text/javascript' src='grid/utils/zapatec.js'></script> -->
+<script type='text/javascript' src='grid/utils/zapatec.js'></script>
 
 	<!-- Custom includes -->	
-<script type="text/javascript" src="grid/src/zpgrid.js"></script>
+<!--<script type="text/javascript" src="grid/src/zpgrid.js"></script>
 <script type="text/javascript" src="grid/demor.js"></script> -->
 
 	<!-- ALL demos need these css -->
