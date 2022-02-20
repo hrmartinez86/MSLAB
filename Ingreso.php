a439cd085b3452ac3b46a349570f5a07aff6eb27
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-02-20 16:16:35 -0600

    change grid

--- Ingreso.php
+++ Ingreso.php
@@ -24,7 +24,7 @@ if(isset($_GET['FI']) and isset($_GET['ff']))
 <meta name="description" content="" />
 <link rel="stylesheet" type="text/css" href="WEB/Styles/Core/Style_doctype.css">
 	<!-- Common JS files-->
-<script type='text/javascript' src='grid/utils/zapatec.js'></script>
+<!-- <script type='text/javascript' src='grid/utils/zapatec.js'></script> -->
 
 	<!-- Custom includes -->	
 <script type="text/javascript" src="grid/src/zpgrid.js"></script>
