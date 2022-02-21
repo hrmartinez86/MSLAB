debf3879614d65a4da9d457ff673fd5d9b908c05
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-02-20 22:59:43 -0600

    exp

--- WEB/Atenciones.php
+++ WEB/Atenciones.php
@@ -70,11 +70,29 @@ $fecha = date('d/m/Y');
     </div>
     <div class="container p-3 my-3 border">
       <div class="form-group-lg">
-        <label for="expediente">Expediente</label>
-        <input type="text" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="expediente">
-        
-        <label for=""></label>
-        <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="">
+        <div class="container">
+          <form>
+            <div class="form-group row">
+              <label for="expediente" class="col-sm-1-12 col-form-label"></label>
+              <div class="col-sm-1-12">
+                <input type="text" class="form-control" name="expediente" id="expediente" placeholder="expediente">
+              </div>
+            </div>
+            <fieldset class="form-group row">
+              <legend class="col-form-legend col-sm-1-12">Group name</legend>
+              <div class="col-sm-1-12">
+                
+              </div>
+            </fieldset>
+            <div class="form-group row">
+              <div class="offset-sm-2 col-sm-10">
+                <button type="submit" class="btn btn-primary">Action</button>
+              </div>
+            </div>
+          </form>
+        </div>
+       
+        <input type="text" class="form-control" name="nombre" id="nombre" aria-describedby="emailHelpId" placeholder="">
         
       </div>
     </div>
