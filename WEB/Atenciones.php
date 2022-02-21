71a0042683485de0db4719196af2efdc4f8821be
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-02-20 23:25:36 -0600

    edad

--- WEB/Atenciones.php
+++ WEB/Atenciones.php
@@ -83,26 +83,8 @@ $fecha = date('d/m/Y');
             <option value="F">Femenino</option>
           </select>
         </div>
-        <div class="container">
-          <form>
-            <div class="form-group row">
-              <label for="inputName" class="col-sm-1-12 col-form-label"></label>
-              <div class="col-sm-1-12">
-                <input type="text" class="form-control" name="inputName" id="inputName" placeholder="">
-              </div>
-            </div>
-            <fieldset class="form-group row">
-              <legend class="col-form-legend col-sm-1-12">Group name</legend>
-              <div class="col-sm-1-12">
-                
-              </div>
-            </fieldset>
-            <div class="form-group row">
-              <div class="offset-sm-2 col-sm-10">
-                <button type="submit" class="btn btn-primary">Action</button>
-              </div>
-            </div>
-          </form>
+        <div class="form-group">
+          <input type="text" name="edad" id="edad" class="form-control" placeholder="Edad del paciente" aria-describedby="helpId">
         </div>
       </div>
     </div>
