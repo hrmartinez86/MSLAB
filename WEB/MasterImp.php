a46c8ef03dbc215cf783720fa912c227b159f3c2
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-05-22 14:55:47 -0500

    espacios en blanco

--- WEB/MasterImp.php
+++ WEB/MasterImp.php
@@ -302,11 +302,11 @@ protected $y0;      // Ordenada de comienzo de la columna
                 $this->Ln($interLinell);
                 //evaluamos si cuenta con un metodo
                 if ($examArray[$i]['metodo']!="") {
-                    $this->WriteText($examArray[$i]['metodo'],10,$interLine,'',$sizeFont,$font,false,false);
+                    $this->WriteText($examArray[$i]['metodo'],15,$interLine,'',$sizeFont,$font,false,false);
                     $this->Ln($interLinell);
                 }
                 else{
-                    $this->Ln($interLinell);
+                    $this->Ln($interLinell+3);
                 }
             }
         }
