6346cf78e8fbf1264c6dd3bacd3fa8ba5e30a130
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-07-30 11:20:45 -0500

    modificaciones a los estudios

--- WEB/EstudiosXseccion.php
+++ WEB/EstudiosXseccion.php
@@ -12,7 +12,7 @@
     $x=0;
     $sql="SELECT lrp.llave_perfil as llave,ccf.nombre as nombre from caj_codigos_fonasa  ccf INNER JOIN lab_relac_fonasa_perfil lrfp ON ccf.llave_fonasa=lrfp.llave_fonasa
     INNER JOIN lab_RLS_perfiles lrp on lrfp.llave_perfil=lrp.llave_perfil
-    where cod_llave='HEMHEM'";
+    where cod_llave='QUIQUI'";
     $query_result=odbc_exec($conection,$sql) or 
                 die ("ERROR : No se puede ejecutar la consulta.");
                 while($result=odbc_fetch_array($query_result))
