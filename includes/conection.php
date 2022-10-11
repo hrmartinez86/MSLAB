5a6f4a1ba15e0255b50d5af5a0091d77f40b5900
hrmartinez86 <hrmartinez86@gmail.com>
2022-10-10 22:30:47 -0500

    chande connection mode

--- includes/conection.php
+++ includes/conection.php
@@ -2,12 +2,19 @@
 
 function conectar()
 {
-$conection = odbc_connect("laboratorio", "sa", "Demo123.") or die ("<h1 align='center'>ERROR EN LA IDENTIFICACION DE USUARIO EN LA ASE DE DATOS FAVOR DE CONSULTAR A SU PROVEEDOR</h1><br> <a href=\"index.php\" title=\"Regresar a la pagina principal\" target=\"_self\"><center>REGRESAR</center></a>");
-return $conection;
+// $conection = odbc_connect("laboratorio", "sa", "Demo123.") or die ("<h1 align='center'>ERROR EN LA IDENTIFICACION DE USUARIO EN LA ASE DE DATOS FAVOR DE CONSULTAR A SU PROVEEDOR</h1><br> <a href=\"index.php\" title=\"Regresar a la pagina principal\" target=\"_self\"><center>REGRESAR</center></a>");
+    $host='192.168.56.1';
+    $dbName='laboratorio';
+    $username='sa';
+    $password='Demo123.';
+    $puerto=1433;
+    try {
+        $conection=new PDO("sqlsrv:Server=$host,$puerto;Database=$dbName",$username,$password);
+        echo("se conecto correctamente la base de datos");
+        return $conection;
+    } catch (PDOException $exp) {
+        echo ("no se conecto la base de datos:$dbName,error $exp");
+    }
 }
-
-
-
-
 
 ?>
--- librerias/conection.php
+++ librerias/conection.php
@@ -2,11 +2,23 @@
 
 function conectar()
 {
-$conection =  odbc_connect("laboratorio", "sa", "Demo123.") or die ("<h1 align='center'>ERROR EN LA IDENTIFICACION DE USUARIO EN LA BASE DE DATOS FAVOR DE CONSULTAR A SU PROVEEDOR</h1><br> 
-<a href=\"index.php\" title=\"Regresar a la pagina principal\" target=\"_self\"><center>REGRESAR</center></a>
-<br>
-<p>".odbc_errormsg()."</p>");
-return $conection;
+// $conection =  odbc_connect("laboratorio", "sa", "Demo123.") or die ("<h1 align='center'>ERROR EN LA IDENTIFICACION DE USUARIO EN LA BASE DE DATOS FAVOR DE CONSULTAR A SU PROVEEDOR</h1><br> 
+// <a href=\"index.php\" title=\"Regresar a la pagina principal\" target=\"_self\"><center>REGRESAR</center></a>
+// <br>
+// <p>".odbc_errormsg()."</p>");
+// return $conection;
+$host='192.168.56.1';
+$dbName='laboratorio';
+$username='sa';
+$password='Demo123.';
+$puerto=1433;
+try {
+    $conection=new PDO("sqlsrv:Server=$host,$puerto;Database=$dbName",$username,$password);
+    echo("se conecto correctamente la base de datos");
+    return $conection;
+} catch (PDOException $exp) {
+    echo ("no se conecto la base de datos:$dbName,error $exp");
+}
 }
 
 ?>
